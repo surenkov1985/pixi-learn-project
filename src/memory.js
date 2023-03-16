26d196ac76af331357bf8dd729089294cae8f29f
mich-man <88162731+surenkov1985@users.noreply.github.com>
2023-03-17 00:08:55 +0300

    обработка кликов по карточкам

--- src/memory.js
+++ src/memory.js
@@ -44,11 +44,11 @@ let DEFAULT_CARD_WIDTH = 100;
 let DEFAULT_CARD_HEIGHT = 160;
 let DEFAULT_LEVEL = 1;
 let level = DEFAULT_LEVEL;
-const DEFAULT_CARD_MASK = { rows: 2, cols: 2 };
 
 let clickCount = 0;
-let selectedCard = null;
-let isSelected = false
+let firstCard = null;
+let secondCard = null;
+let isSelected = false;
 
 const MONSTER_TYPES = {
 	monster1: { texture: monster1Texture },
@@ -72,15 +72,15 @@ const MONSTER_TYPES = {
 const levelsOptions = {
 	1: {
 		monstersNumber: 2,
-		rowsDiff: 1,
-		colsDiff: 1,
+		rows: 2,
+		cols: 2,
 		cardScale: 1,
 		DefaultTime: 60,
 	},
 	2: {
 		monstersNumber: 4,
-		rowsDiff: 1,
-		colsDiff: 2,
+		rows: 2,
+		cols: 4,
 		cardScale: 1,
 		DefaultTime: 60,
 	},
@@ -94,12 +94,18 @@ app.stage.addChild(gameContainer);
 // Контейнер для фона
 const BgContainer = new Container();
 gameContainer.addChild(BgContainer);
+// контейнеры для монстров
+const monstersContainer = new Container();
+monstersContainer.x = app.screen.width / 2;
+monstersContainer.y = app.screen.height / 2;
+gameContainer.addChild(monstersContainer);
+
 // Контейнеры для карточек
 const cardsContainer = new Container();
 cardsContainer.x = app.screen.width / 2;
 cardsContainer.y = app.screen.height / 2;
-cardsContainer.pivot.x = cardsContainer.width / 2;
-cardsContainer.pivot.y = cardsContainer.height / 2;
+// cardsContainer.pivot.x = cardsContainer.width / 2;
+// cardsContainer.pivot.y = cardsContainer.height / 2;
 gameContainer.addChild(cardsContainer);
 
 // /////////////////////// GAMEPLAY ////////////////////////////////////////////////
@@ -112,30 +118,8 @@ BgContainer.addChild(background);
 
 // ///////////////////////////////////
 
-function createShirtCard(type) {
-	const shirtCard = new Sprite(shirtCardTexture);
-
-	shirtCard.alpha = 1;
-	shirtCard.monsterType = type;
-	shirtCard.active = false
-	shirtCard.selected = false
-
-	shirtCard.disable = function () {
-		this.active = false
-		this.visible = false;
-		this.selected = false
-	};
-
-	shirtCard.enable = function () {
-		this.active = true
-		this.visible = true;
-	};
-
-	return shirtCard;
-}
-
 function spawnTypes(number) {
-	const arr = []
+	const arr = [];
 
 	for (let i = 1; i <= number; i++) {
 		const monster = `monster${i}`;
@@ -145,47 +129,74 @@ function spawnTypes(number) {
 	return arr.sort(() => Math.random() - 0.5);
 }
 
+function createShirtCard(type) {
+	const shirtCard = new Sprite(shirtCardTexture);
+
+	shirtCard.width = DEFAULT_CARD_WIDTH;
+	shirtCard.height = DEFAULT_CARD_HEIGHT;
+	shirtCard.alpha = 1;
+	shirtCard.monsterType = type;
+	shirtCard.active = false;
+	shirtCard.selected = false;
+	shirtCard.interactive = true;
+	shirtCard.on("pointerdown", clickCardHandler);
+
+	cardsContainer.addChild(shirtCard);
+
+	shirtCard.disable = function () {
+		this.active = false;
+		this.visible = false;
+		this.selected = false;
+	};
+
+	shirtCard.enable = function () {
+		this.active = true;
+		this.visible = true;
+		this.alpha = 1;
+	};
+
+	return shirtCard;
+}
+
+function createdShirtCardAt(x, y, type) {
+	const card = Pool.getShirtCard(type);
+	card.x = x;
+	card.y = y;
+}
+
 function buildCards(level) {
-	const rows = DEFAULT_CARD_MASK.rows * level.rowsDiff;
-	const cols = DEFAULT_CARD_MASK.cols * level.colsDiff;
+	const rows = level.rows;
+	const cols = level.cols;
 
 	const monsters = spawnTypes(level.monstersNumber);
 
 	for (let i = 0; i < rows; i++) {
-		const cardWidth = DEFAULT_CARD_WIDTH;
-		const cardHeight = DEFAULT_CARD_HEIGHT;
-
-		let posY = (i % rows) * (cardHeight + 20);
+		let posY = (i % rows) * (DEFAULT_CARD_HEIGHT + 20);
 
 		for (let j = 0; j < cols; j++) {
+			const posX = (j % cols) * (DEFAULT_CARD_WIDTH + 20);
 			const monster = monsters.shift();
-			const card = Pool.getShirtCard(monster);
-
-			card.monsterType = monster;
-			card.width = cardWidth;
-			card.height = cardHeight;
-			card.interactive = true;
-			card.x = (j % cols) * (card.width + 20);
-			card.y = posY;
-			card.on("pointerdown", clickCardHandler);
-			cardsContainer.addChild(card);
+			createdShirtCardAt(posX, posY, monster);
 		}
 	}
 
 	cardsContainer.pivot.x = cardsContainer.width / 2;
 	cardsContainer.pivot.y = cardsContainer.height / 2 - 20;
-	console.log(Pool);
+	monstersContainer.pivot.x = cardsContainer.pivot.x;
+	monstersContainer.pivot.y = cardsContainer.pivot.y;
 }
 
 function createMonsterCard(type) {
-	console.log(type);
 	const texture = MONSTER_TYPES[type]["texture"];
 	const monsterCard = new Sprite(texture);
 
+	monsterCard.width = DEFAULT_CARD_WIDTH;
+	monsterCard.height = DEFAULT_CARD_HEIGHT;
+	monsterCard.monsterType = type;
 	monsterCard.alpha = 1;
 	monsterCard.active = false;
 
-	cardsContainer.addChild(monsterCard)
+	monstersContainer.addChild(monsterCard);
 
 	monsterCard.disable = function () {
 		this.active = false;
@@ -201,26 +212,30 @@ function createMonsterCard(type) {
 }
 
 function createMonsterCardAt(x, y, type) {
-	const card = Pool.getMonsterCard(type)
+	const card = Pool.getMonsterCard(type);
 
-	card.width = DEFAULT_CARD_WIDTH
-	card.height = DEFAULT_CARD_HEIGHT
-	card.x = x
-	card.y = y
+	card.x = x;
+	card.y = y;
 }
 
-
 function clickCardHandler() {
-	if (isSelected) return
+	if (isSelected) return;
 	this.selected = true;
-	isSelected = true
-	// const posX = this.x
-	// const posY = this.y
-	
-	// createMonsterCardAt(posX, posY, this.monsterType)
+	isSelected = true;
+
+	if (!firstCard) {
+		firstCard = this.monsterType;
+	} else if (!!firstCard && !secondCard) {
+		secondCard = this.monsterType;
+	}
+
+	console.log(firstCard === secondCard, monstersContainer.children);
+	// if (monstersContainer.children.length === 2) {
+	// 	console.log(monstersContainer.children);
+	// }
 	// if (clickCount > 0) {
 	// 	if (selectedCard && selectedCard.monsterType === this.monsterType) {
-	// 		let posX = 
+	// 		let posX =
 	// 		selectedCard.disable();
 	// 		this.disable();
 	// 		console.log(true, selectedCard, clickCount);
@@ -240,25 +255,55 @@ function clickCardHandler() {
 
 // //////////////////////////////////// ОБНОВЛЕНИЕ ///////////////////////////////
 
-app.ticker.add(delta => {
-	if (isSelected) {
+app.ticker.add((delta) => {
+	// if (isSelected) {
 		for (let i = cardsContainer.children.length - 1; i >= 0; i--) {
-			const card = cardsContainer.children[i]
-			
+			const card = cardsContainer.children[i];
+// console.log(card);
 			if (card && card.selected) {
-				card.alpha -= 0.05 * delta
+				card.alpha -= 0.05 * delta;
 
 				if (card.alpha <= 0) {
 					isSelected = false;
 					const posX = card.x;
 					const posY = card.y;
 					createMonsterCardAt(posX, posY, card.monsterType);
-					card.disable()
+					card.disable();
 				}
 			}
 		}
-	}
-})
+		if (firstCard && secondCard) {
+			isSelected = false;
+			for (let i = monstersContainer.children.length - 1; i >= 0; i--) {
+				if (firstCard === secondCard) {
+					const card = monstersContainer.children[i];
+					card.alpha -= 0.05 * delta;
+					console.log(firstCard === secondCard, monstersContainer.children.length);
+
+					if (card.alpha <= 0) {
+						const posX = card.x;
+						const posY = card.y;
+						// createMonsterCardAt(posX, posY, card.monsterType);
+						card.disable();
+						// isSelected = false
+						firstCard = null;
+						secondCard = null;
+					}
+				} else {
+					console.log(monstersContainer.children[i]);
+					const card = monstersContainer.children[i];
+					const posX = card.x;
+					const posY = card.y;
+					createdShirtCardAt(posX, posY, card.monsterType);
+					card.disable();
+					firstCard = null;
+					secondCard = null;
+					isSelected = false;
+				}
+			}
+		}
+	// }
+});
 
 // ////////////////////////////////////  POOL  ///////////////////////////////////
 
@@ -268,9 +313,9 @@ const Pool = {
 		const key = type + "_shirt";
 		return this.getFromCache(key, () => createShirtCard(type));
 	},
-	getMonsterCard: function(type) {
-		const key = type + "_card"
-		return this.getFromCache(key, () => createMonsterCard(type))
+	getMonsterCard: function (type) {
+		const key = type + "_card";
+		return this.getFromCache(key, () => createMonsterCard(type));
 	},
 
 	getFromCache: function (key, callback) {
@@ -308,6 +353,5 @@ const Pool = {
 		return item;
 	},
 };
-
 
 buildCards(levelsOptions[level]);
